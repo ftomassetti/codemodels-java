db0dcb6bc91a8b997fd41414436e9afd9b337493
Federico Tomassetti <f.tomassetti@gmail.com>
2013-08-27 12:24:27 +0200

    Model building functionalities exported in lightmodels and used here

--- lib/java_model/java_models_builder.rb
+++ lib/java_model/java_models_builder.rb
@@ -20,41 +20,11 @@ $SRC  = sources_path
 $DEST = models_path
 $VERBOSE = false
 
-def translate_dir(src,dest,src_extension,dest_extension,&translate_file)
-	puts "== #{src} -> #{dest} ==" if $VERBOSE
-	Dir["#{src}/*"].each do |fd|		
-		if File.directory? fd
-			basename = File.basename(fd)
-			translate_dir("#{src}/#{basename}","#{dest}/#{basename}",src_extension,dest_extension,&translate_file)
-		else
-			if File.extname(fd)==".#{src_extension}"
-				translated_simple_name = "#{File.basename(fd, ".#{src_extension}")}.#{dest_extension}"
-				translated_name = "#{dest}/#{translated_simple_name}"
-				puts "* #{fd} --> #{translated_name}" if $VERBOSE
-				translate_file.call(fd,translated_name)
-			end
-		end
-	end
-end
-
 $resource_set = create_resource_set()
 
-translate_dir($SRC,$DEST,'java','json') do |src,dest|
-	if not File.exist? dest 
-		puts "<Model from #{src}>"
-	
-		java_resource = get_resource($resource_set, src)
-
-		raise "wrong number of roots" unless java_resource.contents.size == 1
-		root = java_resource.contents.get(0)
-
-		$nextId = 1
-		res = jsonize_java_obj(root)
-
-		dest_dir = File.dirname(dest)
-		FileUtils.mkdir_p(dest_dir) 
-		File.open(dest, 'w') do |file| 		
-			file.write(JSON.pretty_generate(res,:max_nesting => 500))
-		end
-	end
+LightModels.generate_models_in_dir($SRC,$DEST,'java','json') do |src|
+	java_resource = get_resource($resource_set, src)
+	raise "wrong number of roots" unless java_resource.contents.size == 1
+	root = java_resource.contents.get(0)
+	LightModels.eobject_to_model(root)
 end
