f37304a6ff1d2be17fb76479e82de4a2ccaad0ce
Federico Tomassetti <f.tomassetti@gmail.com>
2013-08-27 13:12:10 +0200

    customizable max nesting for json serialization

--- lib/java_model/model_building.rb
+++ lib/java_model/model_building.rb
@@ -5,8 +5,8 @@ module JavaModel
 
 @resource_set = JavaModel.create_resource_set()
 
-def self.generate_java_models_in_dir(src,dest,model_ext='java.lm')
-	LightModels::ModelBuilding.generate_models_in_dir(src,dest,'java',model_ext) do |src|
+def self.generate_java_models_in_dir(src,dest,model_ext='java.lm',max_nesting=500)
+	LightModels::ModelBuilding.generate_models_in_dir(src,dest,'java',model_ext,max_nesting) do |src|
 		java_resource = JavaModel.get_resource(@resource_set, src)
 		raise "wrong number of roots" unless java_resource.contents.size == 1
 		root = java_resource.contents.get(0)
