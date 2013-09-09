6993e70da4944846b67a760fc9e18d596aba493f
Federico Tomassetti <f.tomassetti@gmail.com>
2013-09-09 13:47:48 +0200

    Added handle_models_in_dir method and refactoring to avoid duplication

--- lib/java-lightmodels/model_building.rb
+++ lib/java-lightmodels/model_building.rb
@@ -5,21 +5,28 @@ module JavaModel
 
 @resource_set = JavaModel.create_resource_set()
 
-def self.generate_java_models_in_dir(src,dest,model_ext='java.lm',max_nesting=500)
+MODULE_PRODUCER = Proc.new do |src|
+	java_resource = JavaModel.get_resource(@resource_set, src)
+	raise "wrong number of roots" unless java_resource.contents.size == 1
+	root = java_resource.contents.get(0)
+	LightModels::Serialization.eobject_to_model(root,JavaModel::ADAPTERS_MAP)
+end
+
+def self.generate_models_in_dir(src,dest,model_ext='java.lm',max_nesting=500)
 	LightModels::ModelBuilding.generate_models_in_dir(src,dest,'java',model_ext,max_nesting) do |src|
-		java_resource = JavaModel.get_resource(@resource_set, src)
-		raise "wrong number of roots" unless java_resource.contents.size == 1
-		root = java_resource.contents.get(0)
-		LightModels::Serialization.eobject_to_model(root,JavaModel::ADAPTERS_MAP)
+		MODULE_PRODUCER.call(src)
 	end
 end
 
-def self.generate_java_model_per_file(src,dest,model_ext='java.lm',max_nesting=500)
+def self.generate_model_per_file(src,dest,model_ext='java.lm',max_nesting=500)
 	LightModels::ModelBuilding.generate_model_per_file(src,dest) do |src|
-		java_resource = JavaModel.get_resource(@resource_set, src)
-		raise "wrong number of roots" unless java_resource.contents.size == 1
-		root = java_resource.contents.get(0)
-		LightModels::Serialization.eobject_to_model(root,JavaModel::ADAPTERS_MAP)
+		MODULE_PRODUCER.call(src)
+	end
+end
+
+def self.handle_models_in_dir(src,error_handler=nil,model_handler)
+	LightModels::ModelBuilding.handle_models_in_dir(src,'rb',error_handler,model_handler) do |src|
+		MODULE_PRODUCER.call(src)
 	end
 end
 
