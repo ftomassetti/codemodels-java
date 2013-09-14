d3a5143078512416ce988d804d02344cc0158b39
ftomassetti <FTomassetti@FTOMASSETTI-PC>
2013-09-14 13:36:01 +0200

    Testing correspondences

--- lib/java-lightmodels/parser.rb
+++ lib/java-lightmodels/parser.rb
@@ -13,9 +13,40 @@ java_import 'japa.parser.JavaParser'
 java_import 'java.io.FileInputStream'
 java_import 'java.io.ByteArrayInputStream'
 
+def self.containment_pos(node)
+	container = node.eContainer
+	children  = node.eContainer.send(node.eContainingFeature)
+	if children.respond_to?(:each)
+		children.each_with_index do |c,i|
+			return i if c==node
+		end
+		raise "Not found"
+	else
+		raise "Not found" unless children==node
+		0
+	end
+end
+
 # node tree contains the original 
 def self.corresponding_node(model_element,node_tree)
-	puts "I should get the parent of me #{mode}"
+	return node_tree unless model_element.eContainer
+	corresponding_parent_node = corresponding_node(model_element.eContainer,node_tree)
+	containment_pos = containment_pos(model_element)
+	containing_feat = model_element.eContainingFeature
+
+	children = corresponding_parent_node.send(containing_feat)
+	if children.respond_to?(:each)
+		children[containment_pos]
+	else
+		children
+	end
+end
+
+def self.node_tree_from_code(code)
+	sis = ByteArrayInputStream.new(code.to_java_bytes)
+	node_tree = JavaParser.parse(sis)
+	sis.close
+	node_tree
 end
 
 def self.corresponding_node_from_code(model_element,code)
@@ -32,11 +63,8 @@ def self.parse_file(path)
 	node_to_model(root)
 end
 
-def self.parse_code(code)
-	sis = ByteArrayInputStream.new(code.to_java_bytes)
-	root = JavaParser.parse(sis)
-	sis.close
-	node_to_model(root)
+def self.parse_code(code)	
+	node_to_model(node_tree_from_code(code))
 end
 
 private
---
+++ test/test_correspondences.rb
@@ -0,0 +1,31 @@
+require 'java-lightmodels'
+require 'json'
+
+require "test/unit"
+require "test_helper"
+ 
+class TestCorrespondences < Test::Unit::TestCase
+
+  include TestHelper
+  include LightModels
+  include LightModels::Java
+
+  class << self
+    include JavaModel
+  end
+
+  def setup
+    
+  end
+
+  def test_correspondance_of_field
+    code = "class A extends B { int fieldB; int getB(){ return fieldB; } }"
+    nt = Java.node_tree_from_code(code)
+    mt = Java.parse_code(code)
+    model_field = mt.types[0].members[0]
+    node_field  = nt.types[0].members[0]
+    assert_equal node_field, Java.corresponding_node(model_field,nt)
+    assert_equal "int fieldB;", Java.corresponding_node(model_field,nt).to_s 
+  end
+
+end
