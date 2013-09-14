848bcf3f42c4f6392d1261baf1f2f3f820cca180
ftomassetti <FTomassetti@FTOMASSETTI-PC>
2013-09-14 13:37:33 +0200

    Testing correspondences

--- test/test_correspondences.rb
+++ test/test_correspondences.rb
@@ -28,4 +28,14 @@ class TestCorrespondences < Test::Unit::TestCase
     assert_equal "int fieldB;", Java.corresponding_node(model_field,nt).to_s 
   end
 
+  def test_corresponding_node_from_code
+    code = "class A extends B { int fieldB; int getB(){ return fieldB; } }"
+    nt = Java.node_tree_from_code(code)
+    mt = Java.parse_code(code)
+    model_field = mt.types[0].members[0]
+    node_field  = nt.types[0].members[0]
+    assert_equal node_field, Java.corresponding_node_from_code(model_field,code)
+    assert_equal "int fieldB;", Java.corresponding_node_from_code(model_field,code).to_s
+  end
+
 end
